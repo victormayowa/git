6c2eb831f10678c397e6bcc2cf6e8c1d160804e5
victormayowa <hardmanvicky01@gmail.com>
2023-02-24 21:12:50 +0100

    practicing react tutorial

---
+++ react_practce/src/App.js
@@ -0,0 +1,13 @@
+import React from "react"; 
+import SongList from "./components/SongList";
+import "./App.css";
+
+const App = () => { 
+  return ( 
+    <div className="App"> 
+      <SongList />
+    </div> 
+  ); 
+} 
+ 
+export default App; 
---
+++ react_practce/src/components/SongList.js
@@ -0,0 +1,28 @@
+import React, {useState} from "react";
+import uuid from "uuid/v1";
+import NewSongForm from "./NewSongForm";
+
+
+const SongList = () => {
+    const [songs, setSongs] = useState([
+        {title: 'almost home', id: 1},
+        {title: 'memory godpel', id: 2},
+        {title: 'the wild', id: 3},
+    ]);
+    const addSong = (title) => {
+        setSongs([...songs, {title, id: uuid()}]);
+    }
+    return (
+        <div className="song-list">
+            <ul>
+                {songs.map(song => {
+                    return ( <li key={song.id}>{song.title}</li>);
+                })}
+            </ul>
+            <NewSongForm addSong={addSong}/>
+
+        </div>
+    )
+}
+
+export default SongList;
